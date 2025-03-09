d2beccaf4b7306b49b065eb4ee3f6e974b3d4917
Sireen Ibnu Kabeer <sireenibnukabeer@gmail.com>
2025-03-09 12:46:43 +0530

    Completed Settings page
    - created the settings page ui
    - implemented theme switch logics

--- lib/settings/view/pages/settings_page.dart
+++ lib/settings/view/pages/settings_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:stratagile_todo_test/core/extensions/theme_extension.dart';
+import 'package:stratagile_todo_test/core/theme/theme.dart';
 
 class SettingsPage extends ConsumerWidget {
   const SettingsPage({super.key});
@@ -10,9 +12,72 @@ class SettingsPage extends ConsumerWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          CupertinoSliverNavigationBar(largeTitle: Text("Settings")),
-          SliverToBoxAdapter(),
+          CupertinoSliverNavigationBar(
+            backgroundColor: context.colorScheme.primaryContainer,
+            brightness: context.theme.brightness,
+            largeTitle: Text(
+              "Settings",
+              style: context.typography.headline.copyWith(
+                color: context.colorScheme.onPrimaryContainer,
+              ),
+            ),
+          ),
+          SliverMainAxisGroup(
+            slivers: [
+              SliverAppBar(
+                primary: false,
+                pinned: true,
+                title: Text("Theme", style: context.typography.title),
+              ),
+              SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                sliver: SliverToBoxAdapter(
+                  child: Column(
+                    children: [
+                      _buildSelectThemeTile(
+                        context,
+                        ref,
+                        theme: ThemeMode.light,
+                      ),
+                      _buildSelectThemeTile(
+                        context,
+                        ref,
+                        theme: ThemeMode.dark,
+                      ),
+                      _buildSelectThemeTile(
+                        context,
+                        ref,
+                        theme: ThemeMode.system,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
+      ),
+    );
+  }
+
+  ListTile _buildSelectThemeTile(
+    BuildContext context,
+    WidgetRef ref, {
+    required ThemeMode theme,
+  }) {
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      title: Text(switch (theme) {
+        ThemeMode.system => 'System',
+
+        ThemeMode.light => 'Light',
+
+        ThemeMode.dark => 'Dark',
+      }, style: context.typography.body),
+      trailing: Switch(
+        value: ref.watch(themeNotifierProvider).themeMode == theme,
+        onChanged:
+            (_) => ref.read(themeNotifierProvider.notifier).changeTheme(theme),
       ),
     );
   }
